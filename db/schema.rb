fda33a794f5a5fef85a0310b34a1e8d01b95e3ef
aki-9110 <reokun119@gmail.com>
2025-05-07 15:49:55 +0900

    booksテーブルにratingカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_25_072841) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_07_064314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,5 +19,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_25_072841) do
     t.string "author", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
   end
 end
